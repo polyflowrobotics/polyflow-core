a69f6c3b748eff1898bb121ec3c0a61e01cb6aa9
Drew Swinney <drewswinney@gmail.com>
2026-04-27 20:16:31 -0400

    Updates to diff drive node

--- orchestrators/differential_drive/differential_drive/kernel.py
+++ orchestrators/differential_drive/differential_drive/kernel.py
@@ -1,3 +1,5 @@
+import math
+
 from common.polyflow_kernel import PolyflowKernel
 
 
@@ -33,13 +35,18 @@ class DifferentialDriveKernel(PolyflowKernel):
         return max(-self.max_wheel_speed, min(self.max_wheel_speed, value))
 
     def _emit_motor_commands(self, left_speed: float, right_speed: float):
+        # Clamp linear wheel velocity (m/s), then convert to angular deg/s so
+        # the motor_controller / SAB chain (which assumes deg/s for rotational
+        # joints) lands at the correct physical rad/s on the joint.
         left_speed = self._clamp(left_speed)
         right_speed = self._clamp(right_speed)
+        left_deg_s = left_speed / self.wheel_radius * 180.0 / math.pi
+        right_deg_s = right_speed / self.wheel_radius * 180.0 / math.pi
 
-        self.emit("front_left_motor", {"data": left_speed})
-        self.emit("rear_left_motor", {"data": left_speed})
-        self.emit("front_right_motor", {"data": right_speed})
-        self.emit("rear_right_motor", {"data": right_speed})
+        self.emit("front_left_motor", {"data": left_deg_s})
+        self.emit("rear_left_motor", {"data": left_deg_s})
+        self.emit("front_right_motor", {"data": right_deg_s})
+        self.emit("rear_right_motor", {"data": right_deg_s})
 
     def process_input(self, pin_id: str, data: dict):
         if not self.should_run(trigger_info={"pin_id": pin_id}):
